066f01255083f69a0b831ef330211218c41d78d3
Josef Benninghoff <jsbenning4@gmail.com>
2018-12-21 20:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,4 +51,11 @@ class Trip {
       }.bind(this)
     );
   }
+  passenger(){
+    return store.passengers.find(
+      function(passenger){
+        return passenger.id == this.passengerId;
+      }.bind(this)
+    );
+  }
 }
